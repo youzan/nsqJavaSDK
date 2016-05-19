a66c8e088288dac6ea5683ff7f5ceddbc5f453e2
zhaoxi <zhaoxi@qima-inc.com>
2016-05-19 16:13:00 +0800

    NSQ Frame implements

--- src/main/java/com/youzan/nsq/client/network/frame/ErrorFrame.java
+++ src/main/java/com/youzan/nsq/client/network/frame/ErrorFrame.java
@@ -1,6 +1,13 @@
 package com.youzan.nsq.client.network.frame;
 
+import java.io.UnsupportedEncodingException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class ErrorFrame extends NSQFrame {
+
+    private static final Logger logger = LoggerFactory.getLogger(ErrorFrame.class);
 
     @Override
     public FrameType getType() {
@@ -9,6 +16,16 @@ public class ErrorFrame extends NSQFrame {
 
     @Override
     public String getMessage() {
-        return null;
+        try {
+            return new String(getData(), DEFAULT_CHARSET_NAME);
+        } catch (UnsupportedEncodingException e) {
+            logger.error("Exception", e);
+            return new String(getData());
+        }
+    }
+
+    @Override
+    public String toString() {
+        return getMessage();
     }
 }
--- src/main/java/com/youzan/nsq/client/network/frame/MessageFrame.java
+++ src/main/java/com/youzan/nsq/client/network/frame/MessageFrame.java
@@ -1,11 +1,38 @@
 package com.youzan.nsq.client.network.frame;
 
-public class MessageFrame extends NSQFrame {
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
 
+public class MessageFrame extends NSQFrame {
+    // *************************************************************************
+    // NSQ the message format
+    // *************************************************************************
+    /**
+     * 8-byte : nanosecond timestamp (int64)
+     */
     private byte[] timestamp = new byte[8];
+    /**
+     * 2-byte : (uint16)
+     */
     private byte[] attempts = new byte[2];
+    /**
+     * 16-byte : (hex string encoded in ASCII)
+     */
     private byte[] messageID = new byte[16];
+    /**
+     * N-byte : (binary)
+     */
     private byte[] messageBody;
+
+    @Override
+    public void setData(byte[] bytes) {
+        super.setData(bytes);
+        ByteBuf buf = Unpooled.wrappedBuffer(bytes);
+        buf.readBytes(timestamp);
+        buf.readBytes(attempts);
+        buf.readBytes(messageID);
+        messageBody = buf.readBytes(buf.readableBytes()).array();
+    }
 
     @Override
     public FrameType getType() {
@@ -14,6 +41,6 @@ public class MessageFrame extends NSQFrame {
 
     @Override
     public String getMessage() {
-        return null;
+        throw new UnsupportedOperationException();
     }
 }
