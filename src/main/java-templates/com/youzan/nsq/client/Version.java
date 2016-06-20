b4285913ccf357254ccc7ee37fc7432e9ea8c946
zhaoxi <zhaoxi@qima-inc.com>
2016-06-20 14:24:50 +0800

    Retrieve SDK version from pom

---
+++ src/main/java-templates/com/youzan/nsq/client/Version.java
@@ -0,0 +1,14 @@
+
+package com.youzan.nsq.client;
+
+/**
+ * @author <a href="mailto:my_email@email.exmaple.com">zhaoxi (linzuxiong)</a>
+ *
+ */
+public final class Version {
+
+    public static final String VERSION = "${project.version}";
+    public static final String GROUP_ID = "${project.groupId}";
+    public static final String ARTIFACT_ID = "${project.artifactId}";
+
+}
