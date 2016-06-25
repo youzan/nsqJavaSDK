e1ed36ef7252a5c1dfea3b1da45475b7ddfdea72
zhaoxi <zhaoxi@qima-inc.com>
2016-06-25 10:55:24 +0800

    Separate UTs and ITs: LookupService

--- src/test/java/com/youzan/nsq/client/core/lookup/TestNSQLookupService.java
+++ src/test/java/com/youzan/nsq/client/core/lookup/TestNSQLookupService.java
@@ -3,16 +3,12 @@ package com.youzan.nsq.client.core.lookup;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.SortedSet;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.testng.Assert;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
-
-import com.youzan.nsq.client.entity.Address;
-import com.youzan.nsq.client.exception.NSQLookupException;
 
 public class TestNSQLookupService {
     private static final Logger logger = LoggerFactory.getLogger(TestNSQLookupService.class);
@@ -56,15 +52,6 @@ public class TestNSQLookupService {
             srv = new LookupServiceImpl(ips);
             Assert.assertEquals(srv.getAddresses(), expected);
         } finally {
-        }
-    }
-
-    @Test
-    public void lookup() throws NSQLookupException {
-        LookupServiceImpl srv = new LookupServiceImpl("127.0.0.1:4161");
-        SortedSet<Address> addresses = srv.lookup("test", true);
-        for (final Address addr : addresses) {
-            logger.info("Address : {}", addr);
         }
     }
 }
---
+++ src/test/java/com/youzan/nsq/client/it/ITNSQLookupService.java
@@ -0,0 +1,25 @@
+package com.youzan.nsq.client.it;
+
+import java.util.SortedSet;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.Test;
+
+import com.youzan.nsq.client.core.lookup.LookupServiceImpl;
+import com.youzan.nsq.client.entity.Address;
+import com.youzan.nsq.client.exception.NSQLookupException;
+
+public class ITNSQLookupService {
+
+    private static final Logger logger = LoggerFactory.getLogger(ITNSQLookupService.class);
+
+    @Test
+    public void lookup() throws NSQLookupException {
+        LookupServiceImpl srv = new LookupServiceImpl("127.0.0.1:4161");
+        SortedSet<Address> addresses = srv.lookup("test", true);
+        for (final Address addr : addresses) {
+            logger.info("Address : {}", addr);
+        }
+    }
+}
