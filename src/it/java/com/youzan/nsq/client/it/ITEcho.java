fc9fbbcbcfff1cbccbd83074d0fc44c8b102f452
zhaoxi <zhaoxi@qima-inc.com>
2016-06-25 08:29:30 +0800

    IT

---
+++ src/it/java/com/youzan/nsq/client/it/ITEcho.java
@@ -0,0 +1,10 @@
+package com.youzan.nsq.client.it;
+
+import org.testng.annotations.Test;
+
+public class ITEcho {
+    @Test
+    public void echo() {
+        System.out.println("Integration Tests");
+    }
+}
---
+++ src/test/java/com/youzan/nsq/client/demo/EchoTest.java
@@ -0,0 +1,11 @@
+package com.youzan.nsq.client.demo;
+
+import org.testng.annotations.Test;
+
+public class EchoTest {
+
+    @Test
+    public void echo() {
+        System.out.println("Unit Tests");
+    }
+}
