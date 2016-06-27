f48a165d02d1b188f3fb589539e223b949dc61ef
zhaoxi <zhaoxi@qima-inc.com>
2016-06-27 14:06:29 +0800

    MessageFrame UTs

--- src/test/java/com/youzan/nsq/client/network/frame/TestMessageFrame.java
+++ src/test/java/com/youzan/nsq/client/network/frame/TestMessageFrame.java
@@ -1,14 +1,23 @@
 package com.youzan.nsq.client.network.frame;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class TestMessageFrame {
     @Test
     public void newInstance() {
+        MessageFrame frame = new MessageFrame();
     }
 
     @Test
     public void setData() {
+        MessageFrame frame = new MessageFrame();
+        int size = 10;
+        byte[] bytes = new byte[size];
+        frame.setSize(10);
+        frame.setData(bytes);
+        byte[] actual = frame.getData();
+        Assert.assertEquals(actual, bytes);
     }
 
 }
