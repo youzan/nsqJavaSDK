ab14724cb5bfa6feaf2740691f0976b93f02159d
zhaoxi <zhaoxi@qima-inc.com>
2016-07-01 08:54:36 +0800

    TestMessageFrame

--- src/test/java/com/youzan/nsq/client/network/frame/TestMessageFrame.java
+++ src/test/java/com/youzan/nsq/client/network/frame/TestMessageFrame.java
@@ -24,7 +24,6 @@ public class TestMessageFrame {
         MessageFrame frame = new MessageFrame();
         int size = 100;
         int dataSize = size - 4;
-
         // size + type + data
         // timestamp + attempts + ID + messageBody
 
@@ -51,6 +50,63 @@ public class TestMessageFrame {
         Assert.assertEquals(frame.getAttempts(), attempts);
         Assert.assertEquals(frame.getMessageID(), messageID);
         Assert.assertEquals(frame.getMessageBody(), messageBody);
+    }
+
+    @Test(expectedExceptions = Exception.class)
+    public void dataException() {
+        MessageFrame frame = new MessageFrame();
+        int size = 29;
+        int dataSize = size - 4;
+        // size + type + data
+        // timestamp + attempts + ID + messageBody
+
+        byte[] timestamp = new byte[8];
+        byte[] attempts = new byte[2];
+        byte[] messageID = new byte[16];
+        byte[] messageBody = new byte[dataSize - 8 - 2 - 16];
+        _r.nextBytes(timestamp);
+        _r.nextBytes(attempts);
+        _r.nextBytes(messageID);
+        _r.nextBytes(messageBody);
+
+        ByteBuffer bb = java.nio.ByteBuffer.allocate(dataSize);
+        bb.put(timestamp);
+        bb.put(attempts);
+        bb.put(messageID);
+        bb.put(messageBody);
+        byte[] data = bb.array();
+
+        frame.setSize(dataSize);
+        frame.setData(data);
+
+    }
+
+    @Test
+    public void dataBoundary() {
+        MessageFrame frame = new MessageFrame();
+        int size = 30;
+        int dataSize = size - 4;
+        // size + type + data
+        // timestamp + attempts + ID + messageBody
+
+        byte[] timestamp = new byte[8];
+        byte[] attempts = new byte[2];
+        byte[] messageID = new byte[16];
+        byte[] messageBody = new byte[dataSize - 8 - 2 - 16];
+        _r.nextBytes(timestamp);
+        _r.nextBytes(attempts);
+        _r.nextBytes(messageID);
+        _r.nextBytes(messageBody);
+
+        ByteBuffer bb = java.nio.ByteBuffer.allocate(dataSize);
+        bb.put(timestamp);
+        bb.put(attempts);
+        bb.put(messageID);
+        bb.put(messageBody);
+        byte[] data = bb.array();
+
+        frame.setSize(dataSize);
+        frame.setData(data);
     }
 
 }
