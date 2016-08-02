ebf95a5a60962425d4de8d74502eebf165520544
zhaoxi <zhaoxi@qima-inc.com>
2016-08-02 12:21:06 +0800

    * Integration Testing

--- src/test/java/it/youzan/nsq/client/ITProducer.java
+++ src/test/java/it/youzan/nsq/client/ITProducer.java
@@ -1,14 +1,13 @@
 package it.youzan.nsq.client;
 
-import java.io.IOException;
 import java.util.Random;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.testng.annotations.BeforeClass;
-import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
+import com.youzan.nsq.client.Producer;
 import com.youzan.nsq.client.entity.NSQConfig;
 import com.youzan.nsq.client.exception.NSQException;
 
@@ -19,14 +18,16 @@ public class ITProducer {
 
     private final Random random = new Random();
     private NSQConfig config;
+    private Producer producer;
 
     @BeforeClass
-    public void init() throws NSQException, IOException {
+    public void init() throws NSQException, Exception {
         logger.info("Now init {} at {} .", this.getClass().getName(), System.currentTimeMillis());
     }
 
-    @DataProvider(name = "topics", parallel = true)
-    public Object[][] createData() {
-        return new Object[][] { { "" }, { "" }, { "" } };
+    public void publish() throws NSQException {
+        // final byte[] message = new byte[64];
+        // random.nextBytes(message);
+        // producer.publish(message, "JavaTesting");
     }
 }
