0d8f888304a43ba3872fc839ae0e2937bbd69097
zhaoxi <zhaoxi@qima-inc.com>
2016-08-10 11:37:51 +0800

    tip

--- src/main/java/com/youzan/nsq/client/exception/NSQDataNodesDownException.java
+++ src/main/java/com/youzan/nsq/client/exception/NSQDataNodesDownException.java
@@ -1,17 +1,16 @@
 /**
- * 
+ *
  */
 package com.youzan.nsq.client.exception;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class NSQDataNodesDownException extends NSQException {
     private static final long serialVersionUID = 2336078064990893992L;
 
-    private static final Logger logger = LoggerFactory.getLogger(NSQDataNodesDownException.class);
-
     public NSQDataNodesDownException() {
         super("SDK has done its best effort! All of the data nodes(server-side) are down! Please check both the client and the server!");
+    }
+
+    public NSQDataNodesDownException(Throwable cause) {
+        super("SDK has done its best effort! All of the data nodes(server-side) are down! Please check both the client and the server!", cause);
     }
 }
