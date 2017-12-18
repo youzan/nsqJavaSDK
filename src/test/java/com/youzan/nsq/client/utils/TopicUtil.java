c917b94f1d5729e380145c3174851e9ea8419d6a
元守 <lulin@youzan.com>
2017-12-18 22:20:13 +0800

    commit according to code review
    Signed-off-by: 元守 <lulin@youzan.com>

--- src/test/java/com/youzan/nsq/client/utils/TopicUtil.java
+++ src/test/java/com/youzan/nsq/client/utils/TopicUtil.java
@@ -17,7 +17,6 @@ import org.testng.annotations.Test;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.*;
 import java.util.concurrent.CountDownLatch;
